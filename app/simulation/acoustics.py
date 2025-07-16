bc418283220f111b0a0a51667814b2582e2b2301
Angel Antezana <a.antezana@pucp.pe>
2025-07-16 14:16:42 -0500

    modelos

--- app/simulation/acoustics.py
+++ app/simulation/acoustics.py
@@ -3,33 +3,46 @@
 # Funciones acústicas auxiliares para simulación de silenciadores
 # --------------------------------------------
 
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+
+# --------------------------------------------
+# Importar librerías necesarias
+# --------------------------------------------
+
 import numpy as np
 
+#====================================================================================================================================
+#====================================================================================================================================
+#====================================================================================================================================
+
+# --------------------------------------------
+# Devuelve el número de onda complejo (modo plano) para modelar pérdidas por absorción.
+# --------------------------------------------
+
 def wavenumber_complex(freq, absorption_coeff, c=343):
-    """
-    Devuelve el número de onda complejo (modo plano) para modelar pérdidas por absorción.
-    """
+
     omega = 2 * np.pi * freq
     alpha = absorption_coeff  # puede ser array o escalar
     return omega / c * (1 + 1j * alpha)
 
+# --------------------------------------------
+# Calcula impedancia superficial aproximada de un revestimiento absorbentenen función de su coeficiente de absorción.
+# --------------------------------------------
+
 def lined_wall_impedance(absorption_coeff, rho0=1.21, c=343):
-    """
-    Calcula impedancia superficial aproximada de un revestimiento absorbente
-    en función de su coeficiente de absorción.
-    """
+
     R = (1 - absorption_coeff) / (1 + absorption_coeff)
     Zs = rho0 * c * (1 + R) / (1 - R)
     return Zs
 
+# --------------------------------------------
+# Calcula la atenuación adicional ΔL por rendijas, según: ΔL(f) = 1.05 * α(f)^1.4 * (P/S)
+# --------------------------------------------
+
 def delta_L_additional(alpha, a, h):
-    """
-    Calcula la atenuación adicional ΔL por rendijas, según fórmula:
-    ΔL(f) = 1.05 * α(f)^1.4 * (P/S)
-    Donde:
-    - P = 2(a + 2h)
-    - S = a * 2h
-    """
+
     P = 2 * (a + 2 * h)
     S = a * 2 * h
     deltaL = 1.05 * (alpha ** 1.4) * (P / S)
